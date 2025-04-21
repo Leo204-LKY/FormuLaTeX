66a24295c666ea33541fe192cdb4a2d0cd8a895a
Leo204-LKY <leo2004526@foxmail.com>
2025-04-21 11:12:46 +0800

    Format existed codes

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electronAPI', {
   sendMessage: (message: string) => ipcRenderer.send('message', message),
@@ -8,9 +8,9 @@ window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector: string, text: string | undefined) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text!;
-  }
+  };
 
   for (const dependency of ['chrome', 'node', 'electron']) {
     replaceText(`${dependency}-version`, process.versions[dependency]);
   }
-})
+});
