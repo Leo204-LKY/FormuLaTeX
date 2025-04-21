9d08775858e6c0c7992f01e80d752b5b6d18bb7e
Leo204-LKY <leo2004526@foxmail.com>
2025-04-21 09:53:55 +0800

    Fix ESLint config bugs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,27 +4,55 @@ import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 import json from "@eslint/json";
 import css from "@eslint/css";
+import vueParser from "vue-eslint-parser";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  // Ignore files
   {
-    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+    ignores: [
+      "**/dist/**",
+      "**/node_modules/**",
+      "**/*.min.js",
+      "**/*.config.js",
+      "**/*.config.mjs",
+      "electron-builder.json",
+      "**/tsconfig*.json"
+    ],
+  },
+
+  // JS/TS universal rules (main process + rendering process)
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
     plugins: { js },
     extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node, // 默认认为主进程环境为 Node.js
+    },
   },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,vue}"],
-    languageOptions: { globals: globals.node },
-  },
+
+  // TS type checking (main process)
   tseslint.configs.recommended,
+
+  // Vue 3
   {
     files: ["**/*.vue"],
     plugins: { vue: pluginVue },
+    rules: pluginVue.configs["flat/essential"].rules ?? {},
     languageOptions: {
+      parser: vueParser,
       ...pluginVue.configs["flat/essential"].languageOptions,
-      parserOptions: { parser: tseslint.parser }
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+        extraFileExtensions: [".vue"]
+      },
+      globals: globals.browser, 
     },
   },
+
+  // JSON
   {
     files: ["**/*.json"],
     plugins: { json },
@@ -43,6 +71,8 @@ export default defineConfig([
     language: "json/json5",
     extends: ["json/recommended"],
   },
+
+  // CSS
   {
     files: ["**/*.css"],
     plugins: { css },
