2920335b89751fc1a240861a405a9216c6a42fe4
Leo204_LKY <57821066+Leo204-LKY@users.noreply.github.com>
2025-04-21 21:52:59 +0800

    Setup ESLint & Prettier + Issue Template (#37)
    * Init Prettier and ESLint
    
    * Finish ESLint configuration
    
    * Fix ESLint config bugs
    
    * Finish Prettier configuration
    
    * Ignore VS Code config folder in formatting and git
    
    .gitignore, .prettierignore, and ESLint ignore
    
    * Format existed codes
    
    * Add editor directories and files in .gitignore
    
    * Add GitHub Action for ESLint check (#33)
    
    * Add ESLint action
    
    * Bug fix
    
    * Testing for irregular code
    
    * Bug fix
    
    * Testing for irregular code
    
    * Revert changes in commit cbe37c1 and 3854e9d
    
    * Bug fix
    
    * Testing for irregular code
    
    * Revert "Testing for irregular code"
    
    This reverts commit ceae7a8c905bfa20886df233810a2f25dc9580c1.
    
    * Update README.md
    
    * Update issue templates
    
    * Update README.md
    
    * Rename job name in eslint.yml

---
+++ eslint.config.mjs
@@ -0,0 +1,95 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import json from '@eslint/json';
+import css from '@eslint/css';
+import vueParser from 'vue-eslint-parser';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  // Ignore files
+  {
+    ignores: [
+      '**/dist/**',
+      '**/node_modules/**',
+      '**/*.min.js',
+      '**/*.config.js',
+      '**/*.config.mjs',
+      'electron-builder.json',
+      '**/tsconfig*.json',
+      '.vscode/**',
+    ],
+  },
+
+  // JS/TS universal rules (main process + rendering process)
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node, // 默认认为主进程环境为 Node.js
+    },
+  },
+
+  // TS type checking (main process)
+  tseslint.configs.recommended,
+
+  // Vue 3
+  {
+    files: ['**/*.vue'],
+    plugins: { vue: pluginVue },
+    rules: pluginVue.configs['flat/essential'].rules ?? {},
+    languageOptions: {
+      parser: vueParser,
+      ...pluginVue.configs['flat/essential'].languageOptions,
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+      globals: globals.browser,
+    },
+  },
+
+  // JSON
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.json5'],
+    plugins: { json },
+    language: 'json/json5',
+    extends: ['json/recommended'],
+  },
+
+  // CSS
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended'],
+  },
+
+  // Prettier config
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx,vue,json,css}'],
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': 'error', // Report prettier as ESLint errors
+    },
+  },
+  eslintPluginPrettierRecommended,
+]);
