6e82da464a954e6aa8b99e6ac984dc203d902dd2
Leo204-LKY <leo2004526@foxmail.com>
2025-04-22 21:34:03 +0800

    feat: get random string function

---
+++ src/server/utils/random-str.ts
@@ -0,0 +1,13 @@
+/**
+ * @description Return random string (A-Z, a-z, 0-9)
+ * @param length The length of the string
+ * @returns Generated random string
+ */
+export function getRandomString(length: number): string {
+  const chars =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return Array.from(
+    { length: length },
+    () => chars[Math.floor(Math.random() * chars.length)]
+  ).join('');
+}
