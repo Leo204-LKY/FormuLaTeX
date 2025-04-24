613c50d584549618a52c6239603da1b592d04e29
Leo204-LKY <leo2004526@foxmail.com>
2025-04-24 11:40:58 +0800

    feat: decrypt db file to temp path

---
+++ src/server/database/temp-db-manager.ts
@@ -0,0 +1,23 @@
+/**
+ * Decrypt SQLite3 file to system temp dir for access
+ */
+import path from 'path';
+import { addTempFile, getSafeTempDir } from '../services';
+import { decryptFile, getRandomStr } from '../utils';
+
+/**
+ * Decrypt SQLite3 file to system temp dir for Prisma access
+ * @param encryptedDbPath Encrypt database file path
+ * @returns Decrypted database file path
+ */
+export async function createTempDecryptedDB(encryptedDbPath: string) {
+  const tempDir = getSafeTempDir();
+  const tempDbName = `decrypted-${getRandomStr(12)}.sqlite`;
+  const tempDbPath = path.join(tempDir, tempDbName);
+
+  // Decrypt database
+  decryptFile(encryptedDbPath, tempDbPath);
+  addTempFile(tempDbPath);
+
+  return tempDbPath; // For Prisma conncetion
+}
