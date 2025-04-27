a9080e8066a6768ddf7a887b998fa4f29cb6b4b0
Leo204-LKY <leo2004526@foxmail.com>
2025-04-27 22:50:43 +0800

    feat(database): formula_tags table queies

--- src/server/database/queries/index.ts
+++ src/server/database/queries/index.ts
@@ -1,4 +1,5 @@
 export { FormulaInterpretationsTable } from './formula-interpretations-queies';
 export { FormulaSessionsTable } from './formula-sessions-queies';
+export { FormulaTagsTable } from './formula-tags-queies';
 export { FormulasTable } from './formulas-queies';
 export { TagsTable } from './tags-queies';
