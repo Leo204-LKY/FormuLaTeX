acf348f2d0bdfe28a6e417f81695968c815b12a3
Leo204-LKY <leo2004526@foxmail.com>
2025-04-28 11:09:10 +0800

    feat(database): formula conversations queies

--- src/server/database/queries/index.ts
+++ src/server/database/queries/index.ts
@@ -1,4 +1,5 @@
 export { FavouritesTable } from './favourites-table';
+export { FormulaConversationsTable } from './formula-conversations-queies';
 export { FormulaInterpretationsTable } from './formula-interpretations-queies';
 export { FormulaSessionsTable } from './formula-sessions-queies';
 export { FormulaTagsTable } from './formula-tags-queies';
