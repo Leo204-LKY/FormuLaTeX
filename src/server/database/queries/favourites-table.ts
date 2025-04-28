be33413b93181e24ce2b90c46a1e052135343476
Leo204-LKY <leo2004526@foxmail.com>
2025-04-28 09:23:42 +0800

    feat(database): favourites table queies

---
+++ src/server/database/queries/favourites-table.ts
@@ -0,0 +1,107 @@
+import { PrismaClient } from '../generated';
+import { getPrismaClient } from '../prisma-client';
+import type { favourites } from '../generated';
+
+/**
+ * `favourites` table queries, only contains static methods
+ */
+export class FavouritesTable {
+  private static readonly PRISMA_CLIENT: PrismaClient = getPrismaClient();
+
+  /**
+   * Get all favourites in favourites table
+   * @returns List of all favourites in favourites table
+   */
+  static async getAll(): Promise<favourites[]> {
+    return FavouritesTable.PRISMA_CLIENT.favourites.findMany();
+  }
+
+  /**
+   * Add a formula to favourite by formula UUID, if already exists, skip and directly return the UUID
+   * @param formulaUuid Formula UUID
+   * @returns Favourite UUID
+   */
+  static async insertByFormulaUuid(formulaUuid: string): Promise<string> {
+    // Check if formula favourite already exist
+    const existingFavourite =
+      await FavouritesTable.PRISMA_CLIENT.favourites.findUnique({
+        where: {
+          formula_id: formulaUuid,
+        },
+        select: {
+          favourite_id: true,
+        },
+      });
+
+    // If exist, directly return favourite UUID
+    if (existingFavourite) {
+      return existingFavourite.favourite_id;
+    }
+
+    // If not exist, create favourite and return favourite UUID
+    const newFavourite = await FavouritesTable.PRISMA_CLIENT.favourites.create({
+      data: { formula_id: formulaUuid },
+      select: { favourite_id: true },
+    });
+
+    return newFavourite.favourite_id;
+  }
+
+  /**
+   * Remove a formula favourite by formula UUID
+   * @param formulaUuid Formula UUID
+   * @returns Removed formula UUID, return null if not in favourite
+   */
+  static async deleteByFormulaUuid(
+    formulaUuid: string
+  ): Promise<string | null> {
+    // Check if favourite exists
+    const existingFavourite =
+      await FavouritesTable.PRISMA_CLIENT.favourites.findUnique({
+        where: {
+          formula_id: formulaUuid,
+        },
+      });
+
+    // If not exist, return null
+    if (!existingFavourite) {
+      return null;
+    }
+
+    // If exist, delete and return favourite UUID
+    await FavouritesTable.PRISMA_CLIENT.favourites.delete({
+      where: { formula_id: formulaUuid },
+    });
+
+    return existingFavourite.favourite_id;
+  }
+
+  /**
+   * Remove a formula favourite by favourite UUID
+   * @param favouriteUuid Formula UUID
+   * @returns Removed formula UUID, return null if not in favourite
+   */
+  static async deleteByFavouriteUuid(
+    favouriteUuid: string
+  ): Promise<string | null> {
+    // Check if favourite exists
+    const existingFavourite =
+      await FavouritesTable.PRISMA_CLIENT.favourites.findUnique({
+        where: {
+          favourite_id: favouriteUuid,
+        },
+      });
+
+    // If not exist, return null
+    if (!existingFavourite) {
+      return null;
+    }
+
+    // If exist, delete and return favourite UUID
+    await FavouritesTable.PRISMA_CLIENT.favourites.delete({
+      where: { favourite_id: favouriteUuid },
+    });
+
+    return existingFavourite.favourite_id;
+  }
+}
--- src/server/database/queries/index.ts
+++ src/server/database/queries/index.ts
@@ -1,3 +1,4 @@
+export { FavouritesTable } from './favourites-table';
 export { FormulaInterpretationsTable } from './formula-interpretations-queies';
 export { FormulaSessionsTable } from './formula-sessions-queies';
 export { FormulaTagsTable } from './formula-tags-queies';
