5790c2654b27331c027516109787695ba6df6907
Leo204-LKY <leo2004526@foxmail.com>
2025-04-29 16:50:35 +0800

    style: add interface description

--- src/server/api/simpletex.ts
+++ src/server/api/simpletex.ts
@@ -9,11 +9,28 @@ const BASE_URL = 'https://server.simpletex.cn/api/latex_ocr_turbo'; // Lightweig
 const APP_ID = '';
 const APP_SECRET = '';
 
+/**
+ * SimpleTex API response interface
+ * Contains necessary data of a SimpleTex response
+ */
 export interface SimpleTexResponse {
+  /**
+   * LaTeX code of a recognized image
+   * @example "E=mc^{2}"
+   */
   latex: string;
-  confidence?: number;
-  requestId?: string;
 
+  /**
+   * The confidence of the formula
+   * @example 0.9272882342338562
+   */
+  confidence?: number;
+
+  /**
+   * ID of this request
+   * @example "tr_xxxxxxxxxx"
+   */
+  requestId?: string;
 }
 
 // Generate headers with signature
