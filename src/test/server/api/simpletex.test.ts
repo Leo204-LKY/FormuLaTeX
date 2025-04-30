42c84bc8f10bc74b9339eb298bfec2c3b48db27e
Leo204-LKY <leo2004526@foxmail.com>
2025-04-30 16:46:31 +0800

    test: update simpletex test to get app id and secret form file

--- src/test/server/api/simpletex.test.ts
+++ src/test/server/api/simpletex.test.ts
@@ -1,15 +1,39 @@
 import path from 'path';
-// import fs from 'fs';
+import fs from 'fs';
 import { convertImageToLatex } from '../../../server/api';
+import {
+  getAppDataDirectory,
+  saveEncryptedJsonConfig,
+} from '../../../server/services';
 
 describe('SimpleTex API Tests', () => {
   const imagePath = path.resolve(__dirname, 'images', 'E=mc^2.jpg');
+  const testConfigPath = path.resolve(__dirname, 'secrets.json');
+  const encryptedConfigPath = path.join(getAppDataDirectory(), 'config.json');
+
+  beforeAll(() => {
+    if (!fs.existsSync(testConfigPath)) {
+      throw new Error(
+        'Test secret file secrets.json not found, copy and rename secret.json.template to secret.json and set secret for testing'
+      );
+    }
+
+    const configJson = fs.readFileSync(testConfigPath, { encoding: 'utf-8' });
+    const configObject = JSON.parse(configJson);
+    saveEncryptedJsonConfig('simpletex', configObject);
+  });
+
+  afterAll(() => {
+    if (fs.existsSync(encryptedConfigPath)) {
+      fs.unlinkSync(encryptedConfigPath);
+    }
+  });
 
   describe('formula recognition', () => {
     it('should correctly recognite the formula as E=mc^{2}', async () => {
       const result = await convertImageToLatex(imagePath);
 
-      expect(result).toEqual('E=mc^{2}');
-    });
+      expect(result.latex).toEqual('E=mc^{2}');
+    }, 15000);
   });
 });
