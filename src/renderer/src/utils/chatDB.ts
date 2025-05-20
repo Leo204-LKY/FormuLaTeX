fe59dbd9fb5a0db35c929d0090e23cd126344588
yc1016 <2449622446@qq.com>
2025-05-20 12:50:07 +0800

    feat: prepare for database uses

---
+++ src/renderer/src/utils/chatDB.ts
@@ -0,0 +1,47 @@
+// utils/chatDB.ts
+import { nanoid } from 'nanoid';
+import { FormulaConversationsTable, MessagesTable } from '../../../server';
+
+import type {
+  Prisma,
+  messages,
+  formula_conversations,
+} from '../../../server/database/generated';
+
+export interface ChatData {
+  id: string;
+  title: string;
+  messages: Array<{ isUser: boolean; text: string }>;
+}
+
+// 获取 Conservation 下的对话消息
+export const getMessages = async (id: string) => {
+  const messages = await MessagesTable.getManyByConversationId(id);
+
+  return messages;
+};
+
+// 创建 Conservation
+export const createNewChatDB = (title: string): ChatData => {
+  const newId = nanoid();
+  return {
+    id: newId,
+    title: title || 'Default Chat',
+    messages: [
+      {
+        isUser: false,
+        text: '你好！请问有什么可以帮助你的？',
+      },
+    ],
+  };
+};
+
+// 修改 Conservation 相关信息
+export const updateConservation = (id: string, new_name: string) => {};
+
+// 获取全部 Conservation
+export const getConservations = async () => {
+  const conversations = await FormulaConversationsTable.getAll();
+
+  return conversations;
+};
---
+++ src/renderer/src/utils/formulaDB.ts
@@ -0,0 +1,42 @@
+import { FormulasTable } from '../../../server';
+// import { FormulaTagsTable } from '../../../server';
+import { FormulaSessionsTable } from '../../../server';
+import type { Prisma } from '../../../server/database/generated';
+
+export const createSession = async () => {
+  const new_session: Prisma.formula_sessionsCreateManyInput = {};
+  const session_id = await FormulaSessionsTable.insertOne(new_session);
+  return session_id;
+};
+
+export const createFormula = async () => {
+  const new_formula: Prisma.formulasCreateManyInput = {
+    session_id: await createSession(),
+    latex_code: 'c = \\sqrt{a^2 + b^2}',
+    recognized_by: 'SimpleTex',
+    created_at: new Date(),
+  };
+  const formula_id = await FormulasTable.insertOne(new_formula);
+  return formula_id;
+};
+
+// 获取全部公式，以供加载
+
+// export const getFormulas = (tag_id: string) => {
+//   // 获取 对应tag下 的formula ids
+//   const tag_formula_ids = FormulaTagsTable.getFormulaIdsByTagId(tag);
+//   // 根据 formula ids 获取全部公式传回
+//   //   const formulas: Array<formulas> = ;
+//   return formulas;
+// };
+
+// 对某个公式的名字/进行修改公式
+
+// export const editFormulas = (formula_id: string, new_name: string) => {
+//   // 获取 对应 formula
+//   const edit_formula = FormulasTable.getUniqueByUuid(formula_id);
+//   edit_formula.name = new_name;
+//   // 更新公式（Update命令）
+//   //   FormulasTable.
+//   return;
+// };
