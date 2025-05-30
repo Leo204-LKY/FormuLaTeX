119f46ed134f17f9c9821baf5a6407549665eaf4
Leo204-LKY <leo2004526@foxmail.com>
2025-05-30 13:19:31 +0800

    feat: add language selection and save setting functionality

--- src/renderer/src/utils/locales.ts
+++ src/renderer/src/utils/locales.ts
@@ -1,21 +1,12 @@
 import { createI18n } from 'vue-i18n';
 import en from '../locales/en.json';
 import zhCN from '../locales/zh-CN.json';
-import type { AppSettingsConfig } from '../../../server';
 
 let i18n: ReturnType<typeof createI18n> | undefined;
 
 export async function getI18n() {
   if (!i18n) {
-    let locale;
-    if (!window.servicesApi.isConfigExist('settings')) {
-      locale = navigator.language || 'en';
-    } else {
-      const settings = (await window.servicesApi.getJsonConfig(
-        'settings'
-      )) as AppSettingsConfig;
-      locale = settings.language || 'en';
-    }
+    const locale = (await window.servicesApi.getAppSetting('language')) || 'en';
 
     i18n = createI18n({
       locale,
